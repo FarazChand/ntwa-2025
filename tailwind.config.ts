4623f19b63bf82fa6261d6302a27f0ac44498beb
Faraz Chand <faraz.m.chand@gmail.com>
2025-10-03 18:30:48 -0400

    Initial commit - clean Next.js app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,34 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#111827", // gray.900
+        secondary: "#4B5563", // gray.600
+        accent: "#D1D5DB", // gray.300
+        background: "#ffffff", // white
+        foreground: "#000000", // black
+        muted: "#F3F4F6", // gray.100
+        border: "#E5E7EB", // gray.200
+        input: "#F9FAFB", // gray.50
+        danger: "#DC2626", // red.600
+
+        dark: {
+          primary: "#DC2626",
+          secondary: "#DC2626",
+          background: "#0f172a", // slate.900
+          foreground: "#f8fafc", // slate.50
+        },
       },
     },
   },
