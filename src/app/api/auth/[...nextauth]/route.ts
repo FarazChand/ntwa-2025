0e8e42bd954cf86ce22f33c16605026754655db6
Faraz Chand <faraz.m.chand@gmail.com>
2025-10-05 21:34:11 -0400

    added prisma schema, next auth config for credentials login, docker config

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,59 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compare } from "bcryptjs";
+
+import { prisma } from "@/lib/prisma";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing email or password");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user)
+          throw new Error(
+            "No user found. Please verify your email before logging in.",
+          );
+
+        const isValid = await compare(credentials.password, user.password);
+        if (!isValid) throw new Error("Invalid password");
+
+        return { id: user.id, email: user.email };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.id = user.id;
+      return token;
+    },
+    async session({ session, token }) {
+      //   if (token && session.user) {
+      //     session.user.id = token.id;
+      //   }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-primary text-lg">Hello world!</h1>
-      <h1 className="text-secondary text-lg">Hello world!</h1>
-      <h1 className="text-accent text-lg">Hello world!</h1>
-      <h1 className="text-background text-lg">Hello world!</h1>
-      <h1 className="text-foreground text-lg">Hello world!</h1>
-      <h1 className="text-muted text-lg">Hello world!</h1>
-      <h1 className="text-border text-lg">Hello world!</h1>
-      <h1 className="text-input text-lg">Hello world!</h1>
-      <h1 className="text-danger text-lg">Hello world!</h1>
+      <h1 className="flex text-lg text-primary">Hello world!</h1>
+      <h1 className="flex text-lg text-secondary">Hello world!</h1>
+      <h1 className="text-lg text-accent">Hello world!</h1>
+      <h1 className="text-lg text-background">Hello world!</h1>
+      <h1 className="text-lg text-foreground">Hello world!</h1>
+      <h1 className="text-lg text-muted">Hello world!</h1>
+      <h1 className="text-lg text-border">Hello world!</h1>
+      <h1 className="text-lg text-input">Hello world!</h1>
+      <h1 className="text-lg text-danger">Hello world!</h1>
     </main>
   );
 }
